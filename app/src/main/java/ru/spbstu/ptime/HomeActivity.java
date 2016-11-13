bede35c6e61452678b25fc9fea15a745f13ae4f5
Nick Yakuba <nick.yakuba@gmail.com>
2016-11-14 00:37:40 +0300

    sample drag and drop constructor ui

--- app/src/main/java/ru/spbstu/ptime/HomeActivity.java
+++ app/src/main/java/ru/spbstu/ptime/HomeActivity.java
@@ -1,14 +1,18 @@
 package ru.spbstu.ptime;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+import ru.spbstu.ptime.constructor.ConstructorActivity;
+
+public class HomeActivity extends Activity {
 
     Button btnChrono; // объявление кнопки секундомера
+    Button toConstructor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +20,16 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         btnChrono = (Button) findViewById(R.id.btnChrono); // привязка id к кнопке секундомера
+        toConstructor = (Button) findViewById(R.id.btnToConstructor);
     }
 
     public void onChronoClick (View view){ // действие при нажатии на кнопку секундомера
         Intent intent = new Intent(this, ChronoActivity.class); // создание интента для нового класса (activity)
+        startActivity(intent); // запуск созданного
+    }
+
+    public void onToConstructorClick(View view) {
+        Intent intent = new Intent(this, ConstructorActivity.class); // создание интента для нового класса (activity)
         startActivity(intent); // запуск созданного
     }
 }
