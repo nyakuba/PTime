f12a514f5d0cf77f84a06c70702e19fd0e337abe
svetlana <sgaloshina@mail.ru>
2016-11-16 22:21:38 +0300

    Tabs

--- app/src/main/java/ru/spbstu/ptime/HomeActivity.java
+++ app/src/main/java/ru/spbstu/ptime/HomeActivity.java
@@ -1,81 +1,40 @@
 package ru.spbstu.ptime;
 
 import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.util.Log;
 import android.widget.TabHost;
-import android.widget.TabHost.OnTabChangeListener;
 
 import ru.spbstu.ptime.constructor.ConstructorActivity;
 
-public class HomeActivity extends Activity {
-
-    Button btnChrono; // объявление кнопки секундомера
-    Button toConstructor;
+public class HomeActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        btnChrono = (Button) findViewById(R.id.btnChrono); // привязка id к кнопке секундомера
-        toConstructor = (Button) findViewById(R.id.btnToConstructor);
-    }
+        TabHost tabHost = getTabHost();
 
-    public void onChronoClick (View view){ // действие при нажатии на кнопку секундомера
-        Intent intent = new Intent(this, ChronoActivity.class); // создание интента для нового класса (activity)
-        startActivity(intent); // запуск созданного
-    }
-
-    public void onToConstructorClick(View view) {
-        Intent intent = new Intent(this, ConstructorActivity.class); // создание интента для нового класса (activity)
-        startActivity(intent); // запуск созданного
-    }
-}
-
-
-    protected static final String LOG_TAG = "my_tag";
-    TabHost.TabSpec tabSpec;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
-        // инициализация
-        tabHost.setup();
+        TabHost.TabSpec tabSpec;
 
         tabSpec = tabHost.newTabSpec("tag1");
-        tabSpec.setIndicator("Вкладка 1");
-        tabSpec.setContent(R.id.chrono);
+        tabSpec.setIndicator("Секундомер");
+        tabSpec.setContent(new Intent(this, ChronoActivity.class));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tag2");
-        tabSpec.setIndicator("Вкладка 2");
-        tabSpec.setContent(R.id.timer);
+        tabSpec.setIndicator("Конструктор");
+        tabSpec.setContent(new Intent(this, ConstructorActivity.class));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tag3");
-        tabSpec.setIndicator("Вкладка 3");
-        tabSpec.setContent(R.id.prog);
+        tabSpec.setIndicator("Еще раз конструктор :D");
+        tabSpec.setContent(new Intent(this, ConstructorActivity.class));
         tabHost.addTab(tabSpec);
-
-        // вторая вкладка по умолчанию активна
-        tabHost.setCurrentTabByTag("tag1");
-
-        // логгируем переключение вкладок
-        tabHost.setOnTabChangedListener(new OnTabChangeListener() {
-            public void onTabChanged(String tabId) {
-                Log.d(LOG_TAG, "tabId = " + tabId);
-
-            }
-        });
-
     }
-
 }
