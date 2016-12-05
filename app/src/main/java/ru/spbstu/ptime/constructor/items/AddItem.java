363ba6560b0b136fed32573e0102d9f596ec9ba3
Nick Yakuba <nick.yakuba@gmail.com>
2016-12-05 12:51:55 +0300

    use LayoutInflater to obtain AddItem layout

--- app/src/main/java/ru/spbstu/ptime/constructor/items/AddItem.java
+++ app/src/main/java/ru/spbstu/ptime/constructor/items/AddItem.java
@@ -1,8 +1,10 @@
 package ru.spbstu.ptime.constructor.items;
 
 import android.content.DialogInterface;
+import android.support.annotation.LayoutRes;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import ru.spbstu.ptime.R;
 import ru.spbstu.ptime.constructor.ItemAdapter;
 
 /**
@@ -19,24 +22,28 @@ import ru.spbstu.ptime.constructor.ItemAdapter;
  */
 
 public class AddItem implements ListItem {
+    private final @LayoutRes int mLayoutId = R.layout.add_item;
 
     @Override
     public void initializeLayout(final Long id, final ItemAdapter.ViewHolder holder, final ItemAdapter adapter) {
         LinearLayout item = (LinearLayout) holder.mItemLayout;
+        // since we generate the ListItem content dynamically,
+        // we will just recreate layout using LayoutInflater
         item.removeAllViews();
-        Button button = new Button(item.getContext());
-        button.setText("Add new item");
-        button.setOnClickListener(new View.OnClickListener() {
+        LayoutInflater inflater = holder.mInflater;
+        LinearLayout layout = (LinearLayout) inflater.inflate(mLayoutId, item, true);
+
+        Button btnAddNewItem = (Button) layout.findViewById(R.id.btnAddNewItem);
+        btnAddNewItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 adapter.addItem(new StopwatchItem());
                 adapter.notifyDataSetChanged();
             }
         });
-        item.addView(button);
-        Button buttonSave = new Button(item.getContext());
-        buttonSave.setText("Save");
-        buttonSave.setOnClickListener(view -> {
+
+        Button btnSave = (Button) layout.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(view -> {
             final EditText editText = new EditText(item.getContext()); /* TODO: добавить фильтр вводимых символов. */
             editText.setSingleLine();
             new AlertDialog.Builder(item.getContext())
@@ -58,6 +65,5 @@ public class AddItem implements ListItem {
                     })
                     .show();
         });
-        item.addView(buttonSave);
     }
 }
